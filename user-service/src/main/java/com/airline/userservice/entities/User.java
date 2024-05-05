527e0f760287eaf307677b0f66543c4538eb7da7
nikhilgosavi <nikhil.gosavi@pentacor.de>
2024-05-05 16:51:20 +0200

    apply google-java-format code style

--- user-service/src/main/java/com/airline/userservice/entities/User.java
+++ user-service/src/main/java/com/airline/userservice/entities/User.java
@@ -4,22 +4,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.Date;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @Entity
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
-    private String firstName;
-    private String lastName;
-    private Integer age;
-    private Date createdAt;
-    private Date updatedAt;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long userId;
+
+  private String firstName;
+  private String lastName;
+  private Integer age;
+  private Date createdAt;
+  private Date updatedAt;
 }
--- user-service/src/main/java/com/airline/userservice/repositories/UserRepository.java
+++ user-service/src/main/java/com/airline/userservice/repositories/UserRepository.java
@@ -2,5 +2,4 @@ package com.airline.userservice.repositories;
 
 import com.airline.userservice.entities.User;
 
-public interface UserRepository extends BaseRepository<User, Long> {
-}
+public interface UserRepository extends BaseRepository<User, Long> {}
